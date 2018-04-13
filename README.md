03dc4af0f9a8b5365000b3d3641cbd41f30982ba
tricia <pcampbell.edu@gmail.com>
2018-04-13 11:10:42 -0400

    upgrade ac.sh and README

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 # useful scripts
 useful scripts for testing renaming etc for CS courses
 
-I use some for
+## I use some for
 
 * [360-420-DW](360) Java for sci & engineers, testing java source code
 * [420-112-DW](112) Intro to Logic, testing java source code
 * [420-241-DW](241) OS I Linux testing scripts and system & service installation
 * [420-518-DW](518) Android programming, mostly git automation so far
+
+## Not specific to any course 
+
+* [general use scripts](general) General use, ex asciinema recording to date stamped file
+* [just for fun scripts](not-useful) For fun, ex make the leds blink
 
 # useful templates
 
--- general/ac.sh
+++ general/ac.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 # record asciinema to a unique file every tme
 # uses date stamp  then datestamp-count
+# TODO increase limit of 9 per day &/or warn
 #
 # pm campbell
-# 2018-04-11
+# 2018-04-12
 #
+bn=$(basename $0)
 date=$(date +%F)
-fn=$date
-which asciineman > /dev/null
+fn=$date.cast
+which asciinema > /dev/null  2>&1
 if [[ $? -ne 0 ]] ; then
-     echo $0 please install asciinema 
+     echo $bn please install asciinema 
      exit 5
 fi
-if [[ -f $date ]] ; then
+# fault, maximum 9 recordings in a day
+if [[ -f $date.cast ]] ; then
     for i in {1..9} ; do
         echo $i
-        nd=$date-$i
+        nd=$date-$i.cast
         if [[ ! -f $nd ]] ; then
            fn=$nd
            break
         fi
      done
 fi
+echo $bn recording to $fn
+echo asciinema play $fn will playback the cast
 asciinema rec $fn
