ddd8b161b9901d38988c11ca9a54d24caf95366f
PMC <tricia@localhost.localdomain>
2019-06-14 21:49:01 -0400

    fix token exposure

--- 518/gitlab/gitlab-all.sh
+++ 518/gitlab/gitlab-all.sh
@@ -1,4 +1,3 @@
-ORG=Android518-2018
-ACCESS_TOKEN=u-gz8ZVNz592s8D-QetW
+source org.acces_token
 curl --header "PRIVATE-TOKEN: $ACCESS_TOKEN" "https://gitlab.com/api/v4/groups/$ORG/projects" | sed 's/,/\'$'\n''/g' | grep -e 'ssh_url_to_repo*' | cut -d \" -f 4  | xargs -L1 git clone
 
