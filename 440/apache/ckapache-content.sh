48ac36ba352e28dae978462b150815ef079f234d
pcampbell <pcampbell.edu@gmail.com>
2024-03-06 18:17:45 -0500

    fix scripts

--- 440/apache/ckapache-content.sh
+++ 440/apache/ckapache-content.sh
@@ -11,6 +11,9 @@ fi
 for i in  `cat $1` ; do
     wget --tries=1 --timeout=2 $i  -O $i.html
 done
-echo "\ncheck for own content 0 means own, if it shows here it's not own 
+echo -e  "\ncheck for own content 0 means own, if it shows here it's not own "
 grep "Index of /" *.html -c | grep -v "0$"
 grep "Apache2 Ubuntu Default Page" *.html -c |grep -v "0$"
+echo "check for main.html href if it's 0 not own content"
+grep "main.html" *.html -c |grep "0$"
+
