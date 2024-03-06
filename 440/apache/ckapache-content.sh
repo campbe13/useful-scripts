21795d9127fec7c91d4786faad8b92a891995700
P Campbell <pcampbell.edu@gmail.com>
2024-03-06 18:11:16 -0500

    Update ckapache-content.sh

--- 440/apache/ckapache-content.sh
+++ 440/apache/ckapache-content.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # pmcampbell
-# 2021-01-12
-# given a list of hosts, check which are ssh accessible
+# 2023-03-01
+# given a list of hosts, check they deployed their own content
 
 if [[ $# < 1 ]] ; then
    echo $(basename $0) need a file of hosts to check as cli arg
@@ -11,5 +11,6 @@ fi
 for i in  `cat $1` ; do
     wget --tries=1 --timeout=2 $i  -O $i.html
 done
-echo "\ncheck for own content 0 means own"
+echo "\ncheck for own content 0 means own, if it shows here it's not own 
 grep "Index of /" *.html -c | grep -v "0$"
+grep "Apache2 Ubuntu Default Page" *.html -c |grep -v "0$"
